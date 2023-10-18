f0a6263d7195f3d5186a2dc9ef2aafec1f1ed98f
2018302080133 <fw2396@columbia.edu>
2023-10-18 01:24:10 -0400

    participant module change

--- src/participants/participant.module.ts
+++ src/participants/participant.module.ts
@@ -3,9 +3,11 @@ import { ParticipantsController } from './participant.controller';
 import { ParticipantsService } from './participant.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ParticipantEntity } from './models/participant.entity';
+import { UserEntity } from 'src/users/models/user.entity';
+import { EventEntity } from 'src/events/models/event.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ParticipantEntity])],
+  imports: [TypeOrmModule.forFeature([ParticipantEntity, UserEntity, EventEntity])],
   controllers: [ParticipantsController],
   providers: [ParticipantsService],
 })
