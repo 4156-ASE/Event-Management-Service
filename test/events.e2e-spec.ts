35d151b634215f9bba33d3ceb726b49ed49b655b
Haoqing Wang <teresewang2000@gmail.com>
2023-10-19 21:16:52 -0400

    update e2e

--- test/events.e2e-spec.ts
+++ test/events.e2e-spec.ts
@@ -16,9 +16,59 @@ describe('EventsController (e2e)', () => {
       end_time: new Date('December 17, 2023 04:24:00'),
       location: 'columbia',
     },
+    {
+      id: '2',
+      title: 'mock celebration',
+      desc: 'mock event for testing',
+      start_time: new Date('October 20, 2023 10:00:00'),
+      end_time: new Date('October 20, 2023 12:00:00'),
+      location: 'nyu',
+    },
   ];
+
+  const mockEvent1 = {
+    id: '1',
+    title: 'mock birthday',
+    desc: 'mock event for testing',
+    start_time: '2023-12-17T08:24:00.000Z',
+    end_time: '2023-12-17T09:24:00.000Z',
+    location: 'columbia',
+  };
+  const mockEvent2 = {
+    id: '2',
+    title: 'mock celebration',
+    desc: 'mock event for testing',
+    start_time: '2023-10-20T14:00:00.000Z',
+    end_time: '2023-10-20T16:00:00.000Z',
+    location: 'nyu',
+  };
+  const mockEvent3 = {
+    id: '3',
+    title: 'mock party',
+    desc: 'mock event for testing',
+    start_time: '2023-11-17T21:00:00.000Z',
+    end_time: '2023-11-18T00:00:00.000Z',
+    location: 'columbia',
+  };
+
   const mockEventsRepository = {
-    find: jest.fn().mockResolvedValue(mockEvents),
+    find: jest.fn().mockImplementation((arg) => {
+      if (arg == undefined) {
+        return Promise.resolve(mockEvents);
+      } else {
+        for (const item of mockEvents) {
+          if (item.id == arg.where.id) {
+            return Promise.resolve([item]);
+          }
+        }
+        return Promise.resolve([]);
+      }
+    }),
+    save: jest.fn().mockImplementation((event) => {
+
+      mockEvents.push(event);
+      return [Promise.resolve({ message: 'insert successfully' })];
+    }),
   };
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -32,10 +82,59 @@ describe('EventsController (e2e)', () => {
     await app.init();
   });
 
+  it('/events/ (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/events')
+      .send({
+        id: '3',
+        title: 'mock party',
+        desc: 'mock event for testing',
+        start_time: new Date('November 17, 2023 16:00:00'),
+        end_time: new Date('November 17, 2023 19:00:00'),
+        location: 'columbia',
+      })
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .then((response) => {
+        expect(response.body).toEqual({
+          message: 'insert successfully',
+        });
+      });
+  });
+
   it('/events (GET)', () => {
     return request(app.getHttpServer())
       .get('/events')
+      .expect('Content-Type', /json/)
       .expect(200)
-      .expect(mockEvents);
+      .expect([mockEvent1, mockEvent2, mockEvent3]);
+  });
+
+  it('/events/:id1 (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/events/1')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect(mockEvent1);
+  });
+
+  it('/events/:id2 (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/events/2')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect(mockEvent2);
+  });
+
+  it('/events/:id fail (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/events/10')
+      .expect('Content-Type', /json/)
+      .expect(404)
+      .expect({
+        message: 'Event Not Found.',
+        error: 'Not Found',
+        statusCode: 404,
+      });
   });
 });
