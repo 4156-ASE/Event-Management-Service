8f509c8c697c699c3bbc30d2f10ba64a232cc7d2
2018302080133 <fw2396@columbia.edu>
2023-10-25 02:26:16 -0400

    comments for user and participant

--- src/participants/participant.controller.ts
+++ src/participants/participant.controller.ts
@@ -13,17 +13,21 @@ import { ParticipantsService } from './participant.service';
 
 @Controller('participants')
 export class ParticipantsController {
+  // Injecting ParticipantsService
   constructor(private readonly participantsService: ParticipantsService) {}
 
+  // Invite a participant to an event
   @Post(':eventId')
   async inviteParticipant(
-    @Param('eventId') eventId: string,
-    @Body() user: { first_name: string; last_name: string; email: string },
+    @Param('eventId') eventId: string,  // Extract the 'eventId' parameter from the URL
+    @Body() user: { first_name: string; last_name: string; email: string },  // Extract user details from the request body
   ) {
     try {
+      // Use the service method to invite the participant
       await this.participantsService.inviteParticipant(eventId, user);
       return { message: 'Invitations sent successfully.' };
     } catch (e) {
+      // Handle specific exceptions and re-throw them
       if (e instanceof NotFoundException) {
         throw new NotFoundException(e.message);
       } else {
@@ -34,24 +38,28 @@ export class ParticipantsController {
     }
   }
 
+  // Update the details of a participant in an event
   @Patch(':event_id/:pid')
   async updateParticipantDetails(
-    @Param('event_id') eventId: string,
-    @Param('pid') pid: number,
-    @Body() user: { first_name: string; last_name: string; email: string },
+    @Param('event_id') eventId: string,  // Extract event id from URL
+    @Param('pid') pid: number,  // Extract participant id from URL.
+    @Body() user: { first_name: string; last_name: string; email: string },  // Extract user details from request body
   ) {
     try {
+      // Use the service method to update participant details
       const updatedUser = await this.participantsService.updateParticipant(
         eventId,
         pid,
         user,
       );
+      // Return the updated details
       return {
         first_name: updatedUser.first_name,
         last_name: updatedUser.last_name,
         email: updatedUser.email,
       };
     } catch (e) {
+      // Handle specific exceptions
       if (e instanceof NotFoundException) {
         throw new NotFoundException(e.message);
       } else {
@@ -62,12 +70,15 @@ export class ParticipantsController {
     }
   }
 
+  // Delete a participant
   @Delete(':pid')
-  async deleteParticipant(@Param('pid') pid: number) {
+  async deleteParticipant(@Param('pid') pid: number) {  // Extract 'pid' from URL
     try {
+      // Use the service method to delete the participant
       await this.participantsService.deleteParticipant(pid);
       return { message: 'Participant deleted successfully' };
     } catch (e) {
+      // Handle exceptions
       if (e instanceof NotFoundException) {
         throw new NotFoundException(e.message);
       } else {
@@ -78,11 +89,14 @@ export class ParticipantsController {
     }
   }
 
+  // List participants of an event
   @Get(':eventId')
   async listParticipants(@Param('eventId') eventId: string) {
     try {
+      // Fetch the participants using the service method
       const participants =
         await this.participantsService.listParticipants(eventId);
+      // Map over the results to shape the response
       return participants.map((p) => ({
         pid: p.id,
         first_name: p.user.first_name,
@@ -91,6 +105,7 @@ export class ParticipantsController {
         status: p.status,
       }));
     } catch (e) {
+      // Handle exceptions
       if (e instanceof NotFoundException) {
         throw new NotFoundException(e.message);
       } else {
@@ -99,18 +114,21 @@ export class ParticipantsController {
     }
   }
 
+  // Update the status of a participant
   @Patch('respond')
   async updateParticipantStatus(
     @Body('pid') pid: number,
     @Body('status') status: string,
   ) {
     try {
+      // Use the service method to update the participant's status
       await this.participantsService.updateStatus(pid, status);
       return {
         message:
           'Thank you for your response. We look forward to your participation!',
       };
     } catch (e) {
+      // Handle exceptions
       if (e instanceof NotFoundException) {
         throw new NotFoundException(e.message);
       } else {
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -14,14 +14,17 @@ import { RegisterUserDTO, UpdateUserDTO } from './models/user.dto';
 
 @Controller('users')
 export class UserController {
+  // Dependency injection of the UsersService
   constructor(private usersService: UsersService) {}
 
+  // User registration
   @Post('register')
   async register(@Body() user: RegisterUserDTO) {
     await this.usersService.register(user);
     return { message: 'User registered successfully' };
   }
 
+  // User login
   @Post('login')
   @HttpCode(HttpStatus.OK)
   async login(@Body() data: { email: string; password: string }) {
@@ -29,6 +32,7 @@ export class UserController {
     return result;
   }
 
+  // Retrieve user details by their ID
   @Get(':pid')
   async getUser(@Param('pid') pid: number) {
     const user = await this.usersService.getUser(pid);
@@ -36,6 +40,7 @@ export class UserController {
     return result;
   }
 
+  // Update user details by their ID
   @Patch(':pid')
   async updateUser(@Param('pid') pid: number, @Body() user: UpdateUserDTO) {
     await this.usersService.updateUser(pid, user);
@@ -45,6 +50,7 @@ export class UserController {
     };
   }
 
+  // Delete a user by their ID.
   @Delete(':pid')
   async deleteUser(@Param('pid') pid: number) {
     await this.usersService.deleteUser(pid);
