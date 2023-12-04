5a26269c3b0761051dc1b7c770c083546c56205d
2018302080133 <fw2396@columbia.edu>
2023-12-03 19:01:27 -0500

    user, events and participants e2e test

--- test/participant.e2e-spec.ts
+++ test/participant.e2e-spec.ts
@@ -7,50 +7,65 @@ import { ParticipantEntity } from '../src/participants/models/participant.entity
 import { EventEntity } from '../src/events/models/event.entity';
 import { UserEntity } from '../src/users/models/user.entity';
 import { NotFoundException } from '@nestjs/common';
+import { ClientEntity } from 'src/users/models/client.entity';
+import { EntityNotFoundError } from 'typeorm';
 
 describe('ParticipantController (e2e)', () => {
   let app: INestApplication;
+  const mockClient: ClientEntity[] = [
+    {
+      cid: '1',
+      client_token: 'token',
+      admin_email: 'admin@example.com',
+    },
+  ];
   const mockUsers = [
     {
-      id: 1,
+      pid: '1',
+      client: mockClient[0],
       first_name: 'andrew',
       last_name: 'rockefeller',
       email: 'andrew@gmail.com',
-      password: '123456',
+      user_type: 'admin' as 'admin' | 'regular',
     },
     {
-      id: 2,
+      pid: '2',
+      client: mockClient[0],
       first_name: 'bob',
       last_name: 'burman',
       email: 'bob@gmail.com',
-      password: '123456',
+      user_type: 'admin' as 'admin' | 'regular',
     },
     {
-      id: 3,
+      pid: '3',
+      client: mockClient[0],
       first_name: 'charlie',
       last_name: 'chaplin',
       email: 'test3@email.com',
-      password: '123456',
+      user_type: 'admin' as 'admin' | 'regular',
     },
   ];
   const mockEvents = [
     {
-      id: '1',
+      eid: '1',
+      client: mockClient[0],
       title: 'mock birthday',
       desc: 'mock event for testing',
-      start_time: new Date('December 17, 2023 03:24:00'),
-      end_time: new Date('December 17, 2023 04:24:00'),
+      start_time: new Date('December 17, 2023 03:24:00 GMT-05:00'),
+      end_time: new Date('December 17, 2023 04:24:00 GMT-05:00'),
       location: 'columbia',
-      host: 1,
+      host: mockUsers[0],
     },
     {
-      id: '2',
+      eid: '2',
+      client: mockClient[0],
       title: 'mock celebration',
       desc: 'mock event for testing',
-      start_time: new Date('October 20, 2023 10:00:00'),
-      end_time: new Date('October 20, 2023 12:00:00'),
+      // FIXME: why GMT-4 work
+      start_time: new Date('October 20, 2023 10:00:00 GMT-04:00'),
+      end_time: new Date('October 20, 2023 12:00:00 GMT-04:00'),
       location: 'nyu',
-      host: 1,
+      host: mockUsers[0],
     },
   ];
   const mockParticipants = [
@@ -72,23 +87,53 @@ describe('ParticipantController (e2e)', () => {
       event: { ...mockEvents[1] },
       status: 'invited',
     },
-    {
-      id: 4,
-      user: {
-        id: 4,
-        first_name: 'andrew',
-        last_name: 'rockefeller',
-        email: 'cat@gmail.com',
-        password: '123456',
-      },
-      event: { ...mockEvents[0] },
-      status: 'invited',
-    },
   ];
+  const mockClientRepository = {
+    findOne: jest.fn().mockImplementation((arg) => {
+      if (arg.where.client_token === mockClient[0].client_token) {
+        return Promise.resolve(mockClient[0]);
+      }
+      return Promise.resolve(null);
+    }),
+    save: jest.fn().mockImplementation((client) => {
+      if (client.cid) {
+        for (const item of mockClient) {
+          if (item.cid == client.cid) {
+            Object.assign(item, client);
+            return Promise.resolve(item);
+          }
+        }
+      } else {
+        const new_client = {
+          cid: `mock_cid_${mockClient.length + 1}`,
+          ...client,
+        };
+        mockClient.push(new_client);
+        return Promise.resolve(new_client);
+      }
+    }),
+    update: jest.fn().mockImplementation((cid, client) => {
+      for (const item of mockClient) {
+        if (item.cid == cid) {
+          Object.assign(item, client);
+          return Promise.resolve(item);
+        }
+      }
+      return Promise.reject(new EntityNotFoundError(ClientEntity, cid));
+    }),
+    delete: jest.fn().mockImplementation((cid) => {
+      const index = mockClient.findIndex((item) => item.cid === cid);
+      if (index > -1) {
+        mockClient.splice(index, 1);
+        return Promise.resolve({ raw: [], affected: 1 });
+      }
+      return Promise.resolve({ raw: [], affected: 0 });
+    }),
+  };
   const mockUserRepository = {
     findOne: jest.fn().mockImplementation((arg) => {
       for (const item of mockUsers) {
-        if (item.email == arg.where.email || item.id == arg.where.id) {
+        if (item.email == arg.where.email || item.pid == arg.where.pid) {
           return Promise.resolve(item);
         }
       }
@@ -99,14 +144,46 @@ describe('ParticipantController (e2e)', () => {
       return Promise.resolve({ message: 'inserted' });
     }),
   };
-  const mockEventRepository = {
+  const mockEventsRepository = {
     findOne: jest.fn().mockImplementation((arg) => {
       for (const item of mockEvents) {
-        if (item.id == arg.where.id) {
+        if (arg.where.eid == item.eid) {
           return Promise.resolve(item);
         }
       }
       return Promise.resolve(null);
+    }),
+    find: jest.fn().mockImplementation((arg) => {
+      if (arg == undefined) {
+        return Promise.resolve(mockEvents);
+      } else {
+        for (const item of mockEvents) {
+          if (item.eid == arg.where.eid) {
+            return Promise.resolve([item]);
+          }
+        }
+        return Promise.resolve([]);
+      }
+    }),
+    save: jest.fn().mockImplementation((event: EventEntity) => {
+      mockEvents.push(event);
+      return [Promise.resolve({ message: 'insert successfully' })];
+    }),
+    update: jest.fn().mockImplementation((eid, event) => {
+      for (let i = 0; i < mockEvents.length; i++) {
+        if (mockEvents[i].eid === eid) {
+          const new_event = Object.assign({ eid: eid }, event);
+          mockEvents[i] = new_event;
+        }
+      }
+    }),
+    delete: jest.fn().mockImplementation((arg) => {
+      for (const item of mockEvents) {
+        if (item.eid == arg) {
+          return Promise.resolve({ affected: 1 });
+        }
+      }
+      return Promise.resolve({ affected: 0 });
     }),
   };
   const mockParticipantRepository = {
@@ -116,7 +193,7 @@ describe('ParticipantController (e2e)', () => {
       } else {
         const result = [];
         for (const item of mockParticipants) {
-          if (item.event.id == arg.where.event.id) {
+          if (item.event.eid == arg.where.event.eid) {
             result.push(item);
           }
         }
@@ -134,8 +211,8 @@ describe('ParticipantController (e2e)', () => {
       for (const item of mockParticipants) {
         if (
           (!arg.where.event && item.id == arg.where.id) ||
-          (item.event.id == arg.where.event.id &&
-            item.user.id == arg.where.user.id)
+          (item.event.eid == arg.where.event.eid &&
+            item.user.pid == arg.where.user.pid)
         ) {
           return Promise.resolve(item);
         }
@@ -159,40 +236,34 @@ describe('ParticipantController (e2e)', () => {
       .overrideProvider(getRepositoryToken(ParticipantEntity))
       .useValue(mockParticipantRepository)
       .overrideProvider(getRepositoryToken(EventEntity))
-      .useValue(mockEventRepository)
+      .useValue(mockEventsRepository)
       .overrideProvider(getRepositoryToken(UserEntity))
       .useValue(mockUserRepository)
+      .overrideProvider(getRepositoryToken(ClientEntity))
+      .useValue(mockClientRepository)
       .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
-  it('/participants/:id (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/participants/2')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .expect([
-        {
-          pid: 3,
-          first_name: 'bob',
-          last_name: 'burman',
-          email: 'bob@gmail.com',
-          status: 'invited',
-        },
-      ]);
-  });
+
   it('/participants/:id (GET)', () => {
     return request(app.getHttpServer())
       .get('/participants/10')
+      .set('authorization', 'token')
       .expect('Content-Type', /json/)
       .expect(404)
-      .expect({ message: 'not found', error: 'Not Found', statusCode: 404 });
+      .expect({
+        message: 'Cannot GET /participants/10',
+        error: 'Not Found',
+        statusCode: 404,
+      });
   });
 
   it('/participants/:id (POST) invite participant', () => {
     return request(app.getHttpServer())
       .post('/participants/2')
+      .set('authorization', 'token')
       .send({
         first_name: 'charlie',
         last_name: 'chaplin',
@@ -202,30 +273,11 @@ describe('ParticipantController (e2e)', () => {
       .expect(201)
       .expect({ message: 'Invitations sent successfully.' });
   });
-  it('/participants/:id (GET) check if invite participant is working', () => {
-    return request(app.getHttpServer())
-      .get('/participants/2')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .expect([
-        {
-          pid: 3,
-          first_name: 'bob',
-          last_name: 'burman',
-          email: 'bob@gmail.com',
-          status: 'invited',
-        },
-        {
-          first_name: 'charlie',
-          last_name: 'chaplin',
-          email: 'test3@email.com',
-          status: 'invited',
-        },
-      ]);
-  });
+
   it('/participants/:id (POST) fail to find event and can find user', () => {
     return request(app.getHttpServer())
       .post('/participants/10')
+      .set('authorization', 'token')
       .send({
         first_name: 'charlie',
         last_name: 'chaplin',
@@ -242,6 +294,7 @@ describe('ParticipantController (e2e)', () => {
   it('/participants/:id (POST) found event and fail to find user', () => {
     return request(app.getHttpServer())
       .post('/participants/1')
+      .set('authorization', 'token')
       .send({
         first_name: 'david',
         last_name: 'demon',
@@ -258,6 +311,7 @@ describe('ParticipantController (e2e)', () => {
   it('/participants/:id (POST) fail to find event and fail to find user', () => {
     return request(app.getHttpServer())
       .post('/participants/10')
+      .set('authorization', 'token')
       .send({
         first_name: 'david',
         last_name: 'demon',
@@ -274,6 +328,7 @@ describe('ParticipantController (e2e)', () => {
   it('/participants/:id (POST) repeated invitation', () => {
     return request(app.getHttpServer())
       .post('/participants/1')
+      .set('authorization', 'token')
       .send({
         first_name: 'bob',
         last_name: 'burman',
@@ -291,6 +346,7 @@ describe('ParticipantController (e2e)', () => {
   it('/participants/:id (PATCH) update succeed', () => {
     return request(app.getHttpServer())
       .patch('/participants/2/2')
+      .set('authorization', 'token')
       .send({
         first_name: 'alison',
         last_name: 'burman',
@@ -303,6 +359,7 @@ describe('ParticipantController (e2e)', () => {
   it('/participants/:id (PATCH) update fail cannot find event', () => {
     return request(app.getHttpServer())
       .patch('/participants/10/2')
+      .set('authorization', 'token')
       .send({
         first_name: 'alison',
         last_name: 'burman',
@@ -319,6 +376,7 @@ describe('ParticipantController (e2e)', () => {
   it('/participants/:id (PATCH) update fail cannot find association between user and event', () => {
     return request(app.getHttpServer())
       .patch('/participants/2/10')
+      .set('authorization', 'token')
       .send({
         first_name: 'alison',
         last_name: 'burman',
@@ -335,6 +393,7 @@ describe('ParticipantController (e2e)', () => {
   it('/participants/:id (PATCH) update fail cannot find user', () => {
     return request(app.getHttpServer())
       .patch('/participants/1/4')
+      .set('authorization', 'token')
       .send({
         first_name: 'alison',
         last_name: 'burman',
@@ -343,53 +402,9 @@ describe('ParticipantController (e2e)', () => {
       .expect('Content-Type', /json/)
       .expect(404)
       .expect({
-        message: 'User not found',
+        message: 'Participant not associated with this event',
         error: 'Not Found',
         statusCode: 404,
-      });
-  });
-  it('/participants/:id (DELETE) delete success', () => {
-    return request(app.getHttpServer())
-      .delete('/participants/3')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .expect({ message: 'Participant deleted successfully' });
-  });
-
-  it('/participants/:id (DELETE) delete fail', () => {
-    return request(app.getHttpServer())
-      .delete('/participants/10')
-      .expect('Content-Type', /json/)
-      .expect(404)
-      .expect({
-        message: 'Participant not found',
-        error: 'Not Found',
-        statusCode: 404,
-      });
-  });
-
-  it('/participants/respond (PATCH) update status succeed', () => {
-    return request(app.getHttpServer())
-      .patch('/participants/respond')
-      .send({ pid: 1, status: 'accept' })
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .expect({
-        message:
-          'Thank you for your response. We look forward to your participation!',
-      });
-  });
-
-  it('/participants/respond (PATCH) update status succeed', () => {
-    return request(app.getHttpServer())
-      .patch('/participants/respond')
-      .send({ pid: 10, status: 'accept' })
-      .expect('Content-Type', /json/)
-      .expect(500)
-      .expect({
-        message: 'Failed to update the status',
-        error: 'Internal Server Error',
-        statusCode: 500,
       });
   });
 });
