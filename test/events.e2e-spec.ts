4793d99f78f56350dac46bfb008cec45136dac9b
Haorui <hs3374@columbia.edu>
2023-12-06 22:13:21 -0500

    test: add one e2e file

---
+++ test/events.e2e-spec.ts
@@ -0,0 +1,45 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { EventsModule } from '../src/events/events.module';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { EventEntity } from '../src/events/models/event.entity';
+import { EntityNotFoundError } from 'typeorm';
+
+import * as moduleAlias from 'module-alias';
+
+function initAlias() {
+  moduleAlias.addAliases({
+    '@/(.*)': '<rootDit>/$1',
+  });
+}
+
+initAlias();
+
+describe('EventsController (e2e)', () => {
+  let app: INestApplication;
+  const mockEventsRepository = {
+    save: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [EventsModule],
+    })
+      .overrideProvider(getRepositoryToken(EventEntity))
+      .useValue(mockEventsRepository)
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/events (GET)', () => {
+    mockEventsRepository.find.mockResolvedValue([]);
+    return request(app.getHttpServer()).get('/events').expect(200).expect([]);
+  });
+
+});
