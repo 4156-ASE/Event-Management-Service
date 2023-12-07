e7e68d492033a1e870a505dd1b811f2b9aaeb891
Haorui <hs3374@columbia.edu>
2023-12-07 02:14:05 -0500

    feat(email): add sending email notifications test: add jest spy on sending emails

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -7,6 +7,10 @@ import {
   EventDetail,
   EventUpdateReq,
 } from './models/event.dto';
+import * as nodemailer from 'nodemailer';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as handlebars from 'handlebars';
 
 /**
  * Event Service which will handle event relevent database operations, can be used by event controller.
@@ -52,7 +56,38 @@ export class EventsService {
         cid: cid,
       },
     });
-
+    if (this.isValidEmail(data.host_email) && data.host_name) {
+      const replacements = {
+        name: data.host_name,
+        title: data.title,
+        desc: data.desc,
+        startTime: data.start_time,
+        endTime: data.end_time,
+        location: data.location,
+        host: data.host_name,
+        message: 'You have created an event.'
+      };
+      const content = this.loadHTMLContent(replacements);
+      await this.sendEmail([data.host_email], 'Event Created', content);
+    }
+    if (data.participants_email && data.participants_name && data.participants_email.length === data.participants_name.length) {
+      for (let i = 0; i < data.participants_email.length; i++) {
+        if (this.isValidEmail(data.participants_email[i]) && data.participants_name[i]) {
+          const replacements = {
+            name: data.participants_name[i],
+            title: data.title,
+            desc: data.desc,
+            startTime: data.start_time,
+            endTime: data.end_time,
+            location: data.location,
+            host: data.host_name,
+            message: 'You have been invited to an event.'
+          };
+          const content = this.loadHTMLContent(replacements);
+          await this.sendEmail([data.participants_email[i]], 'Event Invitation', content);
+        }
+      }
+    }
     return this.eventEntity2EventDetail(cid, event);
   }
 
@@ -141,6 +176,39 @@ export class EventsService {
       throw new NotFoundException('Not found event');
     }
 
+    if (updatedEvent.host_email && updatedEvent.host_name) {
+      const replacements = {
+        name: updatedEvent.host_name,
+        title: updatedEvent.title,
+        desc: updatedEvent.desc,
+        startTime: updatedEvent.start_time,
+        endTime: updatedEvent.end_time,
+        location: updatedEvent.location,
+        host: updatedEvent.host_name,
+        message: 'You have updated an event.'
+      };
+      const content = this.loadHTMLContent(replacements);
+      await this.sendEmail([updatedEvent.host_email], 'Event Updated', content);
+    }
+    if (updatedEvent.participants_email && updatedEvent.participants_name && updatedEvent.participants_email.length === updatedEvent.participants_name.length) {
+      for (let i = 0; i < updatedEvent.participants_email.length; i++) {
+        if (this.isValidEmail(updatedEvent.participants_email[i]) && updatedEvent.participants_name[i]) {
+          const replacements = {
+            name: updatedEvent.participants_name[i],
+            title: updatedEvent.title,
+            desc: updatedEvent.desc,
+            startTime: updatedEvent.start_time,
+            endTime: updatedEvent.end_time,
+            location: updatedEvent.location,
+            host: updatedEvent.host_name,
+            message: 'You have been invited to an event.'
+          };
+          const content = this.loadHTMLContent(replacements);
+          await this.sendEmail([updatedEvent.participants_email[i]], 'Event Invitation', content);
+        }
+      }
+    }
+
     const event = await this.getEvent(cid, eventID);
 
     return event;
@@ -160,4 +228,53 @@ export class EventsService {
 
     return result.affected === 1;
   }
+
+  /**
+   * Sending email function.
+   * @param {string[]} receivers The receivers' email address.
+   * @param {string} subject The email subject.
+   * @param {string} content The email content.
+   */
+  async sendEmail(receivers: string[], subject: string, content: string){
+    const transporter = nodemailer.createTransport({
+      host: 'smtp-mail.outlook.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'event4156-2@outlook.com',
+        pass: '4156eventmanagement'
+      },
+      tls: {
+        ciphers: 'SSLv3'
+      }
+    });
+    await transporter.sendMail({
+      from: 'event4156-2@outlook.com',
+      to: receivers,
+      subject: subject,
+      html: content
+    });
+  }
+
+  /**
+   * Check if a string is a valid email address.
+   * @param {string} email The email address.
+   */
+  private isValidEmail(email: string) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  /**
+   * Load email html template.
+   */
+  private loadHTMLContent(replacements) {
+    const __dirname = path.resolve();
+    const filePath = path.join(__dirname, 'src/pages/emailPage.html');
+    const source = fs.readFileSync(filePath, 'utf-8').toString();
+    const template = handlebars.compile(source);
+    return template(replacements);
+  }
+
+
 }
